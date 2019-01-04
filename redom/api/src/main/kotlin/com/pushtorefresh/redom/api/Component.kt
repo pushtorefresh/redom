778fbd87e507a18fde991bbed460c2b4c3738a80
Artem Zinnatullin :slowpoke <ceo@artemzin.com>
2019-01-04 03:21:13 -0800

    Dumb implementation of TextView (#4)

--- redom/api/src/main/kotlin/com/pushtorefresh/redom/api/Component.kt
+++ redom/api/src/main/kotlin/com/pushtorefresh/redom/api/Component.kt
@@ -10,4 +10,25 @@ interface Component<O> {
     val initProperties: Map<String, *>
     val observeProperties: Map<String, Observable<*>>
     val changeProperties: Map<String, Observable<*>>
+
+    private data class DComponent<O>(
+            override val clazz: Class<*>,
+            override val output: Observable<O>,
+            override val rawOutput: Observable<*>,
+            override val initProperties: Map<String, *>,
+            override val observeProperties: Map<String, Observable<*>>,
+            override val changeProperties: Map<String, Observable<*>>
+    ) : Component<O>
+
+    companion object {
+        fun <O> create(
+                clazz: Class<*>,
+                output: Observable<O>,
+                rawOutput: Observable<*>,
+                initProperties: Map<String, *>,
+                observeProperties: Map<String, Observable<*>>,
+                changeProperties: Map<String, Observable<*>>
+        ): Component<O> = DComponent(clazz, output, rawOutput, initProperties, observeProperties, changeProperties)
+    }
+
 }
