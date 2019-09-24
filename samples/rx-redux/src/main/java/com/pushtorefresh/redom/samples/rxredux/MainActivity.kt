362d140bcc6f907ae5a837073043ceaa3ab26c29
Sergey Boishtyan <vitilus123@gmail.com>
2019-09-24 12:23:24 +0300

    Close #66; SideEffect like Use Case or Interactor in own file;

--- samples/rx-redux/src/main/java/com/pushtorefresh/redom/samples/rxredux/MainActivity.kt
+++ samples/rx-redux/src/main/java/com/pushtorefresh/redom/samples/rxredux/MainActivity.kt
@@ -1,9 +1,13 @@
 package com.pushtorefresh.redom.samples.rxredux
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.pushtorefresh.redom.samples.rxredux.feed.FeedActivity
 import com.pushtorefresh.redom.samples.rxredux.signin.SignInAndroidView
+import com.pushtorefresh.redom.samples.rxredux.signin.SignInSideEffect
 import com.pushtorefresh.redom.samples.rxredux.signin.SignInStateMachine
+import com.pushtorefresh.redom.samples.rxredux.signin.SignInSuccessfulSideEffect
 import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.rxkotlin.plusAssign
 import io.reactivex.schedulers.Schedulers
@@ -17,7 +21,18 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val view = SignInAndroidView(findViewById(android.R.id.content))
-        val stateMachine = SignInStateMachine(view.actions, RealSignInService(), Schedulers.computation())
+        val stateMachine = SignInStateMachine(
+            inputActions = view.actions,
+            sideEffects = listOf(
+                SignInSideEffect(
+                    signInService = RealSignInService()
+                ),
+                SignInSuccessfulSideEffect {
+                    startActivity(Intent(this, FeedActivity::class.java))
+                }
+            ),
+            reduxLoopScheduler = Schedulers.computation()
+        )
 
         disposable += view.render(stateMachine.state)
     }
